5fde9e02fa6e77dd56e5e6c0f433923f1b8b0575
Javier Alejandro González Casellas <78095527+JalexCode@users.noreply.github.com>
2024-01-04 12:05:34 -0500

    Added Main section

---
+++ src/components/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const Main = () => {
+  return (
+    <div className='bg-indigo-600'>
+      <NavBar />
+      
+    </div>
+  )
+}
+
+export default Main
