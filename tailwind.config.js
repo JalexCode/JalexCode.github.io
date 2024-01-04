ffa7cb0f44c9301183765983d4ba6c4a88641ed4
Javier Alejandro González Casellas <78095527+JalexCode@users.noreply.github.com>
2024-01-04 09:58:46 -0500

    Added tailwind config and screen sizes

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./**/*.tsx"],
+  theme: {
+    screen: {
+      ssm:'375px',
+      sm:'640px',
+      md:'768px',
+      lg:'1024px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
