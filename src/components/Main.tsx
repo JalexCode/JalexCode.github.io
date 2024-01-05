5828970cd2bb74601142ee55338ace81de03358e
Javier Alejandro González Casellas <78095527+JalexCode@users.noreply.github.com>
2024-01-05 02:25:31 -0500

    Added Skills section

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import NavBar from './NavBar'
 import Hero from './Hero'
+import Skills from './Skills'
 
 const Main = () => {
-  return (
-    <div className='bg-indigo-600'>
-      <NavBar />
-      <Hero/>
-    </div>
-  )
+    return (
+        <div className='bg-indigo-600'>
+            <NavBar />
+            <div className='pt-20'>
+                <Hero />
+                <Skills />
+            </div>
+        </div>
+    )
 }
 
 export default Main
