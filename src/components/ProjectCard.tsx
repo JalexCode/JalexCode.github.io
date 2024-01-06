da47ede177d877487e4d04a0dc52625b8543757a
Javier Alejandro González Casellas <78095527+JalexCode@users.noreply.github.com>
2024-01-06 18:01:56 -0500

    Added Animations

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,28 +1,27 @@
-import React from 'react'
-import Project from '../data/projects'
 import ReactMarkdown from 'react-markdown';
 
 interface ProjectCardProps {
     project: Project;
-  }
+}
 
-const ProjectCard = ({project}:ProjectCardProps) => {
+const ProjectCard = ({ project }: ProjectCardProps) => {
     return (
-        <div className='project-card justify-around'>
-            <div className='mr-5 items-center p-4 my-auto w-full h-full'>
-                <img src={project.iconPath} className='rounded-xl'/>
+        <div className='z-1 project-card justify-around p-5 hover-trigger transition-all hover:scale-105 shadow-2xl hover:shadow-4xl '>
+            <div className='flex flex-shrink-0 mr-4'>
+                <img src={project.iconPath} className='h-24 min-h-24 object-cover transition-all hover-target' />
             </div>
-            <div className='flex flex-col p-4 text-start'>
-                <h3 className='font-bold text-xl'>{project.name}</h3>
-                <ReactMarkdown className='font-sans text-md text-slate-500'>{project.markdownDescription}</ReactMarkdown>
-                <div className='grid grid-cols-3 gap-4'>
+            <div className='flex flex-col text-start h-full flex-grow'>
+                <h3 className='font-bold text-xl mb-2'>{project.name}</h3>
+                <ReactMarkdown className='font-sans text-md text-slate-500 mb-4'>{project.markdownDescription}</ReactMarkdown>
+                <div className='text-slate-400 text-sm mt-auto mb-2'>Technologies tags:</div>
+                <div className='flex flex-wrap gap-2'>
                     {project.usedTechs?.map((tech, index) => (
-                        <div key={index} className='w-full mr-3 mt-2 text-center rounded-md bg-slate-400 text-slate-100 text-base p-x-2 p-y-6'>{tech}</div>
+                        <div key={index} className='tag-badges'>{tech}</div>
                     ))}
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default ProjectCard
+export default ProjectCard;
