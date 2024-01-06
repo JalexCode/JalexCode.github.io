4ccc13d51095e251c0d0b6d21cc1a813a96a8010
Javier Alejandro González Casellas <78095527+JalexCode@users.noreply.github.com>
2024-01-06 18:03:17 -0500

    Added lottie Setted up styles

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,18 +1,30 @@
-import ProjectCard from "./ProjectCard"
-import projects from "../data/projects"
+import ProjectCard from "./ProjectCard";
+import projects from "../data/projects";
+import Lottie from 'lottie-react'
+import rocketAnimation from '../assets/lotties/rocket-purple.json';
+
 const Projects = () => {
   return (
-    <section className='bg-white'>
-      <div>
-        <h2 className="sections-title mb-8">Projects</h2>
+    <section className='bg-white p-4 my-52 mx-20 justify-center items-center w-fit relative'>
+      <div className='absolute top-0 left-0 -mt-20 ml-40 z-0'>
+        <Lottie
+          loop={true}
+          autoplay={true}
+          animationData={rocketAnimation}
+          className='opacity-80 animate-fadeInUp'
+          style={{ height: '200px' }}
+        />
       </div>
-      <div className="grid grid-cols-2 ">
+      <div className="z-1">
+        <h2 className="sections-title mb-8 text-2xl font-bold">Projects</h2>
+      </div>
+      <div className="grid grid-cols-2 gap-10 z-1">
         {projects.map((project, index) => (
-          <ProjectCard key={index} project={project}/>
+          <ProjectCard key={index} project={project} />
         ))}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
