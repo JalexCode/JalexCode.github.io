b8a41704fcac765de189925ae0c5623068ad47e3
Javier Alejandro González Casellas <78095527+JalexCode@users.noreply.github.com>
2024-01-06 13:48:51 -0500

    Changed main div by nav Setted navbar as fixed Added blur to navbar Added animation to menu items

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,22 +1,25 @@
 import { AiOutlineClose } from 'react-icons/ai'
+import JalexCode from '../assets/jalex.png'
 const NavBar = () => {
-  const items = ["About Me", "Experience", "Skills", "Projects"]
+  const items = ["About Me", "Skills", "Projects", "Experience"]
   return (
-    <>
-      <div className='bg-indigo-500 p-10 block ssm:space-y-4'>
+    <>    
+      <nav className='fixed bg-indigo-600 px-10 py-5 ssm:space-y-4 w-full backdrop-filter backdrop-blur-lg bg-opacity-50 z-50'>
         <div className="flex justify-between items-center">
           {/* Javier Alejandro */}
-          <div className="text-white font-mono text-3xl flex items-center">Javier Alejandro</div>
+          <div className="text-white font-mono text-3xl flex items-center">
+            <img src={JalexCode} alt='JalexCode' width={250}/>
+          </div>
           {/* Menu Bar */}
           <div className="space-x-4 lg:block ssm:hidden">
             {items.map((element, index) => (
-              <a key={index} className="nav-bar-menu-item">{element}</a>
+              <a key={index} className="dynamic-bg-button">{element}</a>
             ))}
           </div>
           {/* X button to close menu */}
           <div className="lg:hidden">
               <AiOutlineClose size={30} className="text-white" />
-            </div>
+          </div>
         </div>
         {/* Menu items list for responsive design */}
         <div className="flex justify-between ssm:block lg:hidden">
@@ -26,7 +29,7 @@ const NavBar = () => {
             ))}
           </ul>
         </div>
-      </div>
+      </nav>
     </>
   )
 }
