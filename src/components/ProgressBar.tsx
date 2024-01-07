0279d2ff5f42518d99a3b45645080edd12b7381e
Javier Alejandro González Casellas <78095527+JalexCode@users.noreply.github.com>
2024-01-07 01:11:42 -0500

    clasificate function was commented

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -3,17 +3,17 @@ interface ProgressBarProps {
     percent: number;
     visible: boolean;
 }
-function clasificate(rate: number): string {
-    if (rate > 0 && rate < 25) {
-        return "Still learning"
-    } else if (rate > 25 && rate < 50) {
-        return "Good"
-    } else if (rate > 50 && rate < 75) {
-        return "Advanced"
-    } else {
-        return "Pro"
-    }
-}
+// function clasificate(rate: number): string {
+//     if (rate > 0 && rate < 25) {
+//         return "Still learning"
+//     } else if (rate > 25 && rate < 50) {
+//         return "Good"
+//     } else if (rate > 50 && rate < 75) {
+//         return "Advanced"
+//     } else {
+//         return "Pro"
+//     }
+// }
 
 const ProgressBar = ({ title, percent, visible }: ProgressBarProps) => {
     return (
