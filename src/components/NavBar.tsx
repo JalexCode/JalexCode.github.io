501348b00e75c7efb25929f24ea7295ca6b5bd56
Javier Alejandro González Casellas <78095527+JalexCode@users.noreply.github.com>
2024-01-07 16:11:30 -0500

    Added id field to items array Added scrollToSection function

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,24 +1,42 @@
 import { AiOutlineClose } from 'react-icons/ai'
 import JalexCode from '../assets/jalex.png'
+import Link from 'react-scroll'
+
+interface Sections {
+  title:string,
+  id:string,
+}
+
 const NavBar = () => {
-  const items = ["About Me", "Skills", "Projects", "Experience"]
+  const items:Array<Sections> = [
+   {title:"About Me", id: "aboutme"},
+   {title:"Skills", id: "skills"},
+   {title:"Projects", id: "projects"},
+   {title:"Experience", id: "experience"},
+  ]
+  const scrollToSection = (sectionId: string) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
   return (
-    <>    
+    <>
       <nav className='fixed bg-indigo-600 px-10 py-5 ssm:space-y-4 w-full backdrop-filter backdrop-blur-lg bg-opacity-50 z-50'>
         <div className="flex justify-between items-center">
           {/* Javier Alejandro */}
           <div className="text-white font-mono text-3xl flex items-center">
-            <img src={JalexCode} alt='JalexCode' width={250}/>
+            <img src={JalexCode} alt='JalexCode' width={250} />
           </div>
           {/* Menu Bar */}
           <div className="space-x-4 lg:block ssm:hidden">
-            {items.map((element, index) => (
-              <a key={index} className="dynamic-bg-button">{element}</a>
+            {items.map((element:Sections, index) => (
+              <a key={index} className="menu-item" onClick={() => scrollToSection(element.id)}>{element.title}</a>
             ))}
           </div>
           {/* X button to close menu */}
           <div className="lg:hidden">
-              <AiOutlineClose size={30} className="text-white" />
+            <AiOutlineClose size={30} className="text-white" />
           </div>
         </div>
         {/* Menu items list for responsive design */}
