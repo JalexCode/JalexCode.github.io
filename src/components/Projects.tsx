7f836e988873ae743f249fb22acb3c28822a1c2e
Javier Alejandro González Casellas <78095527+JalexCode@users.noreply.github.com>
2024-01-07 18:16:17 -0500

    Added section iud Removed lottie

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,20 +1,9 @@
 import ProjectCard from "./ProjectCard";
 import projects from "../data/projects";
-import Lottie from 'lottie-react'
-import rocketAnimation from '../assets/lotties/rocket-purple.json';
 
 const Projects = () => {
   return (
-    <section className='bg-white p-4 my-52 mx-20 justify-center items-center w-fit relative'>
-      <div className='absolute top-0 left-0 -mt-20 ml-40 z-0'>
-        <Lottie
-          loop={true}
-          autoplay={true}
-          animationData={rocketAnimation}
-          className='opacity-80 animate-fadeInUp'
-          style={{ height: '200px' }}
-        />
-      </div>
+    <section id="projects" className='bg-white px-4 pt-28 my-52 mx-20 justify-center items-center w-fit relative'>
       <div className="z-1">
         <h2 className="sections-title mb-8 text-2xl font-bold">Projects</h2>
       </div>
