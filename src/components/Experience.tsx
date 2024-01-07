45d9fb057a1659cc6fd27b9cc49a6609edbb6c33
Javier Alejandro González Casellas <78095527+JalexCode@users.noreply.github.com>
2024-01-07 18:14:00 -0500

    Added  section id Deleted wrapper div to section title

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,11 +1,10 @@
 import { experience } from '../data/experience'
 import ExperienceCard from './ExperienceCard'
+
 const Experience = () => {
   return (
-    <section className='bg-white p-4 my-52 mx-20 justify-center items-center w-fit relative'>
-      <div className="z-1">
-        <h2 className="sections-title mb-8 text-2xl font-bold">Experience</h2>
-      </div>
+    <section id="experience" className='bg-white pt-28 px-4 my-40 mx-20 justify-center items-center w-fit relative'>
+      <h2 className="sections-title mb-8 text-2xl font-bold">Experience</h2>
       <div className="flex flex-wrap z-1">
         {experience.map((experience, index) => (
         //   <ProjectCard key={index} project={experience} />
