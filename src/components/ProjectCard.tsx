4c86a534cb532a041d756d59a14bb02ad444cd9b
Javier Alejandro González Casellas <78095527+JalexCode@users.noreply.github.com>
2024-01-07 03:33:04 -0500

    Added bottom bar (commented)

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,4 +1,5 @@
 import ReactMarkdown from 'react-markdown';
+// import { AiOutlineGithub } from 'react-icons/ai';
 
 interface ProjectCardProps {
     project: Project;
@@ -6,7 +7,7 @@ interface ProjectCardProps {
 
 const ProjectCard = ({ project }: ProjectCardProps) => {
     return (
-        <div className='z-1 project-card justify-around p-5 hover-trigger transition-all hover:scale-105 shadow-2xl hover:shadow-4xl'>
+        <div className='z-1 project-card justify-around p-5 hover-trigger transition-all hover:scale-105 shadow-2xl hover:shadow-black'>
             <div className='flex flex-shrink-0 mr-4'>
                 <img src={project.iconPath} className='h-24 min-h-24 object-cover transition-all hover-target' />
             </div>
@@ -20,6 +21,9 @@ const ProjectCard = ({ project }: ProjectCardProps) => {
                     ))}
                 </div>
             </div>
+            {/* <div className='absolute bottom-0 left-0 right-0 bg-black justify-center items-center rounded-br-3xl rounded-bl-3xl h-0 transition-all hover-bottom-bar-target'>
+                <AiOutlineGithub className='text-white mx-auto my-auto' size={24} />
+            </div> */}
         </div>
     );
 };
