b49357d3043fcbd06621f9f1a3e659aa4af666c0
Javier Alejandro González Casellas <78095527+JalexCode@users.noreply.github.com>
2024-01-09 01:51:00 -0500

    Style: increm top padding value

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -3,7 +3,7 @@ import ExperienceCard from './ExperienceCard'
 
 const Experience = () => {
   return (
-    <section id="experience" className='bg-white pt-28 px-4 my-40 mx-20 justify-center items-center w-fit relative'>
+    <section id="experience" className='bg-white pt-40 px-4 my-40 mx-20 justify-center items-center w-fit relative'>
       <h2 className="sections-title mb-8 text-2xl font-bold">Experience</h2>
       <div className="flex flex-wrap z-1">
         {experience.map((experience, index) => (
