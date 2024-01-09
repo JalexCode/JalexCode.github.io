082f4f60869a004d561f96757c7d383d24a1a1c4
Javier Alejandro González Casellas <78095527+JalexCode@users.noreply.github.com>
2024-01-09 01:17:38 -0500

    Style: fixed space problems Style: Group name responsive

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -13,14 +13,14 @@ const Skills = () => {
             <div >
               <h2 className="sections-title">Skills</h2>
             </div>
-            <div className="flex lg:flex-row ssm:flex-col w-full lg:space-x-10 ssm:space-y-10 justify-center">
+            <div className="flex lg:flex-row ssm:flex-col w-full lg:space-x-10 ssm:space-x-0 lg:space-y-0 ssm:space-y-10 justify-center z-40">
               {skillGroups.map((group, index) => (
                 <React.Fragment key={index}>
                   <table className="h-fit lg:w-1/4 ssm:w-full">
                     <tbody>
                       <tr>
                         <td>
-                          <h3 className="text-1xl font-semibold text-start text-gray-600">{group.groupName}</h3>
+                          <h3 className="text-1xl font-semibold lg:text-start ssm:text-center text-gray-600">{group.groupName}</h3>
                         </td>
                       </tr>
                       {group.skills.sort((a, b) => b.rating - a.rating).map(skill => (
@@ -39,7 +39,7 @@ const Skills = () => {
               loop={true}
               autoplay={true}
               animationData={frontEndDevAnimation}
-              className='opacity-40 mb-5 mr-5'
+              className='opacity-40 mb-5 mr-5 z-0 ssm:hidden lg:block'
               style={{ width: '200px', bottom: 0, right: 0, position: "absolute" }}
             />
           </div>
