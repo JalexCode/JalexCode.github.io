11bf28fdaa21f77dc3d828f9acf63559e982ae1b
Javier Alejandro González Casellas <78095527+JalexCode@users.noreply.github.com>
2024-01-09 00:42:04 -0500

    Style: Deleted section negative margin Working in make it responsive

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -7,46 +7,45 @@ import frontEndDevAnimation from '../assets/lotties/front-end-lottie-purple.json
 
 const Skills = () => {
   return (
-    <section id="skills" className="-mt-60 mb-28 pt-28">
-      <div className="card my-4 mx-20">
-        <div className="text-center">
-          <div >
-            <h2 className="sections-title">Skills</h2>
-          </div>
-
-          <div className="flex w-full space-x-10 justify-center">
-
-            {skillGroups.map((group, index) => (
-              <React.Fragment key={index}>
-                <table className="h-fit w-1/4">
-                  <tbody>
-                    <tr>
-                      <td>
-                        <h3 className="text-1xl font-semibold text-start text-gray-600">{group.groupName}</h3>
-                      </td>
-                    </tr>
-                    {group.skills.sort((a, b) => b.rating - a.rating).map(skill => (
+    <section id="skills" className="bg-indigo-600 pt-40">
+      <div className="card py-10 lg:mx-20 ssm:mx-10">
+          <div className="text-center">
+            <div >
+              <h2 className="sections-title">Skills</h2>
+            </div>
+            <div className="flex lg:flex-row ssm:flex-col w-full lg:space-x-10 ssm:space-y-10 justify-center">
+              {skillGroups.map((group, index) => (
+                <React.Fragment key={index}>
+                  <table className="h-fit lg:w-1/4 ssm:w-full">
+                    <tbody>
                       <tr>
-                        <td className="flex items-center mt-5">
-                          <SkillItem image={skill.iconPath} text={skill.name} percent={skill.rating} bgColor={skill.color} />
+                        <td>
+                          <h3 className="text-1xl font-semibold text-start text-gray-600">{group.groupName}</h3>
                         </td>
                       </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </React.Fragment>
-            ))}
+                      {group.skills.sort((a, b) => b.rating - a.rating).map(skill => (
+                        <tr>
+                          <td className="flex items-center mt-5">
+                            <SkillItem image={skill.iconPath} text={skill.name} percent={skill.rating} bgColor={skill.color} />
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </React.Fragment>
+              ))}
+            </div>
+            <Lottie
+              loop={true}
+              autoplay={true}
+              animationData={frontEndDevAnimation}
+              className='opacity-40 mb-5 mr-5'
+              style={{ width: '200px', bottom: 0, right: 0, position: "absolute" }}
+            />
           </div>
-          <Lottie
-            loop={true}
-            autoplay={true}
-            animationData={frontEndDevAnimation}
-            className='opacity-40 mb-5 mr-5'
-            style={{ width: '200px', bottom:0, right: 0, position:"absolute"}}
-        />
+
         </div>
-        
-      </div>
+
     </section>
   );
 };
