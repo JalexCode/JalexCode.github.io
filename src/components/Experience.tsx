b1c9b7ec3dbaa7cd7c36ce43104b7d2c39dead45
Javier Alejandro González Casellas <78095527+JalexCode@users.noreply.github.com>
2024-01-15 18:12:25 -0500

    Style: added px values for  different screens

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -3,11 +3,10 @@ import ExperienceCard from './ExperienceCard'
 
 const Experience = () => {
   return (
-    <section id="experience" className='bg-white pt-40 px-4 my-40 mx-20 justify-center items-center w-fit relative'>
-      <h2 className="sections-title mb-8 text-2xl font-bold">Experience</h2>
+    <section id="experience" className='bg-white pt-40 my-40 sssm:px-10 lg:px-20 justify-center items-center w-fit'>
+      <h2 className="sections-title mb-10 text-2xl font-bold">Experience</h2>
       <div className="flex flex-wrap z-1">
         {experience.map((experience, index) => (
-        //   <ProjectCard key={index} project={experience} />
         <ExperienceCard key={index} experience={experience}/>
         ))}
       </div>
