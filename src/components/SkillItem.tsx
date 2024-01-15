4e4933c53993465743d0c67ee062d069a7d82b34
Javier Alejandro González Casellas <78095527+JalexCode@users.noreply.github.com>
2024-01-15 18:16:02 -0500

    Refactor

--- src/components/SkillItem.tsx
+++ src/components/SkillItem.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import ProgressBar from "./ProgressBar";
-// import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 
 interface SkillItemProps {
     image: string // Tipo JSX.Element para el elemento SVG
@@ -14,10 +13,8 @@ const SkillItem = ({ image, text, percent, bgColor }: SkillItemProps) => {
     console.log(bgColor)
     return (
         <div className="skill-item" onClick={()  =>  setPressed(!pressed)}> 
-        {/* {`flex w-full justify-start space-x-4 items-center rounded-lg bg-transparent hover:${bgColor === "blue" ? "bg-blue-100" : bgColor === "green" ? "bg-green-100" : bgColor === "red" ? "bg-red-100" : bgColor === "yellow" ? "bg-yellow-100" : bgColor === "orange" ? "bg-orange-100" : bgColor === "indigo" ? "bg-indigo-100" :  "bg-gray-100"} transition-colors duration-300 ease-in-out px-2 py-2 cursor-pointer`} */}
             <img src={image} className="w-10 h-10 mr-1" />
             <ProgressBar title={text} percent={percent} visible={true}/>
-            {/* {pressed ? <IoIosArrowUp size={30} className={"text-slate-400"}/> : <IoIosArrowDown size={30} className={"text-slate-500"}/>} */}
         </div>
     )
 }
