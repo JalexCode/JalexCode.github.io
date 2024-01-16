249079cd66c68c6bf6fdaa89129d4d9e5cb6064d
Javier Alejandro González Casellas <78095527+JalexCode@users.noreply.github.com>
2024-01-16 00:13:12 -0500

    Style: fixed section title margin bottom

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,8 +4,8 @@ import projects from "../data/projects";
 const Projects = () => {
   return (
     <section id="projects" className='bg-white dark:bg-slate-600 pt-40 sssm:mt-32 sm:mt-32 lg:mt-52 sssm:mx-10 ssm:mx-10 lg:mx-20 justify-center items-center w-fit relative'>
-      <div className="z-1">
-        <h2 className="sections-title mb-8 text-2xl font-bold">Projects</h2>
+      <div className="mb-16">
+        <h2 className="sections-title text-2xl font-bold">Projects</h2>
       </div>
       <div className="sssm:flex ssm:flex lg:grid sssm:flex-col ssm:flex-col lg:grid-cols-2 sssm:gap-24 ssm:gap-24 lg:gap-10 sssm:mt-10 ssm:mt-10 lg:mt-0 z-1">
         {projects.map((project, index) => (
