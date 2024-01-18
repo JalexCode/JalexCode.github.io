2f2509a92594b98483c4ccd3dad59a8aa4c92df8
Javier Alejandro González Casellas <78095527+JalexCode@users.noreply.github.com>
2024-01-18 18:39:07 -0500

    Add: Google Analytics init

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import {DarkModeProvider} from './context/DarkModeContext.tsx'
+import { initGA, logPageView } from './ga.ts';
+
+initGA();
+logPageView();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <DarkModeProvider>
