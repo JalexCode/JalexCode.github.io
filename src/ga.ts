c3052a94f9e118fd048a88fb7dd119b2fa492ef5
Javier Alejandro González Casellas <78095527+JalexCode@users.noreply.github.com>
2024-01-20 12:05:48 -0500

    Add: GoogleTagManager

--- src/ga.ts
+++ src/ga.ts
@@ -1,8 +1,18 @@
 // ga.ts
 import ReactGA from 'react-ga';
+import TagManager from 'react-gtm-module'
+
+const id = 'G-6MZW8JV2DB'
+
+const tagManagerArgs = {
+  gtmId: id
+}
+export const initTM = () => {
+  TagManager.initialize(tagManagerArgs)
+}
 
 export const initGA = () => {
-  ReactGA.initialize('G-6MZW8JV2DB');
+  ReactGA.initialize(id);
 };
 
 export const logPageView = () => {
