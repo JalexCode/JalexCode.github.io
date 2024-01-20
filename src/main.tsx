83096ed1c8802a501d02ce9eab44bccfd89f30cf
Javier Alejandro González Casellas <78095527+JalexCode@users.noreply.github.com>
2024-01-20 12:05:15 -0500

    Add: init Google Tag Manager

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import {DarkModeProvider} from './context/DarkModeContext.tsx'
-import { initGA, logPageView } from './ga.ts';
+import { initGA, initTM, logPageView } from './ga.ts';
 
+initTM();
 initGA();
 logPageView();
 
